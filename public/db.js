734d5d4d8f7feaae14876c7108ac3e5d173b0b7e
slongy7 <79759725+slongy7@users.noreply.github.com>
2021-09-29 19:19:50 -0400

    db update

--- public/db.js
+++ public/db.js
@@ -1,2 +1,17 @@
 let db;
 
+const request = indexedDB.open('budgetdb, 1');
+
+request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+    db.createObjectStore('pending', { autoIncrement: true});
+
+};
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+
+    if (navigator.onLine) {
+        checkDatabase ();
+    }
+};
