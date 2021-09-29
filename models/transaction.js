1ff986208fc934a0716629927752e8563bfaa30d
slongy7 <79759725+slongy7@users.noreply.github.com>
2021-09-29 15:07:00 -0400

    updated

--- models/transaction.js
+++ models/transaction.js
@@ -1,25 +1,25 @@
-const mongoose = require ('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const transactionSchema = new Schema (
-    {
-        name: String,
-        trim: true,
-        required: 'Enter name for transaction'
+const transactionSchema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: "Enter a name for transaction"
     },
-    {
-        value: {
-            type: Number,
-            required: 'Enter an Amount'
-        },
-        date: {
-            type: Date,
-            default: Date.now
-        }
+    value: {
+      type: Number,
+      required: "Enter an amount"
+    },
+    date: {
+      type: Date,
+      default: Date.now
     }
+  }
 );
 
-const Transaction = mongoose.model('Transaction', transactionSchema);
+const Transaction = mongoose.model("Transaction", transactionSchema);
 
 module.exports = Transaction;
